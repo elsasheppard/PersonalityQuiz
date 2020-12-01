12fea4e67865c173967c5cb96de8be9994323540
Elsa Sheppard <sheppard.elsa03@gmail.com>
2020-12-01 12:47:39 -0800

    Start adding option for different languages

--- app/src/main/java/com/example/truefalse/MainActivity.kt
+++ app/src/main/java/com/example/truefalse/MainActivity.kt
@@ -2,12 +2,27 @@ package com.example.truefalse
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.gson.Gson
 
 class MainActivity : AppCompatActivity() {
+
+    private val TAG = "MainActivity"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // opening the raw resource and reading the file as a string
+        val inputStream = resources.openRawResource(R.raw.questions)
+        val inputString = inputStream.bufferedReader().use {
+            it.readText()
+        }
+        Log.d(TAG, "onCreate: " + inputString)
 
+        // parsing the string into our custom objects using Gson
+        val gson = Gson()
+        // use the parsing between the collection, list, or array section of:
+        // https://medium.com/@hissain.khan/parsing-with-google-gson-library-in-android-kotlin-7920e26f5520
     }
 }
